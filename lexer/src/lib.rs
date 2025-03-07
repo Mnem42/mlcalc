5b514cb4adf227eb080d6546e8a52c3b3cf1c14b
Mnem42 <IshaanAbhi25@gmail.com>
2025-03-07 21:09:18 +0000

    idfk what i did

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,4 +1,4 @@
-
+use std::str::SplitWhitespace;
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Keyword{
     Add,
@@ -19,14 +19,14 @@ pub enum Token{
 
 #[derive(Debug)]
 struct Lexer<'a>{
-    data: Vec<&'a str>,
+    data: SplitWhitespace<'a>,
     position: usize
 }
 
 impl Lexer<'_>{
     pub fn new(input: &str) -> Lexer{
         Lexer{
-            data:input.split_whitespace().collect(),
+            data:input.split_whitespace(),
             position:0
         }
     }
@@ -36,23 +36,28 @@ impl Iterator for Lexer<'_>{
     type Item = Token;
 
     fn next(&mut self) -> Option<Token>{
-        self.position+=1;
+        let slice = self.data.next();
 
-        if self.position>self.data.len() {
-            return None;
-        }
+        match slice{
+            Some(x) =>{
+				self.position+=x.chars().count();
 
-        let data=self.data[self.position-1];
+				let float_opt = match x.parse::<f64>() {
+					Ok(x)  => Some(Token::FloatLiteral(x)),
+					Err(_) => None
+				};
 
-        if data.parse::<f64>().is_ok() {
-            Some(Token::FloatLiteral(data.parse::<f64>().expect("???")))
-        }
-        else{
-            match data{
-                "add" => Some(Token::Keyword(Keyword::Add)),
-                "sub" => Some(Token::Keyword(Keyword::Sub)),
-                x     => Some(Token::Unidentified(data.to_string()))
+				if float_opt != None {
+					return float_opt;
+				}
+
+				match x{
+					"add" => Some(Token::Keyword(Keyword::Add)),
+					"sub" => Some(Token::Keyword(Keyword::Sub)),
+					_     => Some(Token::Unidentified(x.to_string()))
+				}
             }
+            None=>None
         }
     }
 }
@@ -62,21 +67,27 @@ mod tests {
     use super::*;
 
     #[test]
-    fn lexer_test(){
-        let lexer = Lexer::new("add 1 2 x");
-        println!("{:?}",lexer);
+    fn lexer_input_test_a(){
+        let mut lexer = Lexer::new("add 1 2 x");
+
         assert!(lexer.eq([Token::Keyword(Keyword::Add),Token::FloatLiteral(1.0),
             Token::FloatLiteral(2.0),Token::Unidentified("x".to_string())]));   
-
-        /*assert_eq!(lexer.next(),Some(Token::Keyword(Keyword::Add)));
-        assert_eq!(lexer.next(),Some(Token::FloatLiteral(1.0)));
-        assert_eq!(lexer.next(),Some(Token::FloatLiteral(2.0)));
-        assert_eq!(lexer.next(),None);*/
     }
 
     #[test]
-    fn lexer_test_map(){
-        let mut lexer = Lexer::new("add 1 2 x \n sub 1 3 x");
-        lexer.for_each(|x| println!("{:?}",x));
+    fn lexer_input_test_b(){
+        let mut lexer = Lexer::new("add 1 2 x \n sub 1 3 x \n foo");
+
+        assert!(lexer.eq([
+            Token::Keyword(Keyword::Add),
+            Token::FloatLiteral(1.0),
+            Token::FloatLiteral(2.0),
+            Token::Unidentified("x".to_string()),
+            Token::Keyword(Keyword::Sub),
+            Token::FloatLiteral(1.0),
+            Token::FloatLiteral(3.0),
+            Token::Unidentified("x".to_string()),
+            Token::Unidentified("foo".to_string())
+        ]));
     }
 }
