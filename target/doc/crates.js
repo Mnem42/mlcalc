281be46bec283f2c51bc10bf66d32d2972753a59
Mnem42 <IshaanAbhi25@gmail.com>
2025-03-09 19:20:05 +0000

    idk

---
+++ target/doc/crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["interpereter","mlcalc"];
+//{"start":21,"fragment_lengths":[14,9]}
---
+++ target/doc/src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["interpereter",["",[],["fileio.rs","lexer.rs","lib.rs"]]],["mlcalc",["",[],["main.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[58,31]}
