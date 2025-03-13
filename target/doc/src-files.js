0f568e593f48abfbbc5bbd6ab7c0bc371942debe
CordlessCoder <roman@vm.net.ua>
2025-03-13 15:27:00 +0000

    Remove target

--- target/doc/src-files.js
+++
@@ -1,3 +0,0 @@
-var srcIndex = new Map(JSON.parse('[["interpereter",["",[],["fileio.rs","lexer.rs","lib.rs","resolver.rs"]]],["interpereter",["",[],["fileio.rs","lexer.rs","lib.rs"]]],["mlcalc",["",[],["main.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[72,59,31]}
